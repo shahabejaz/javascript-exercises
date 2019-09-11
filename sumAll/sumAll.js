2979f1e27c932dc75f924ef36589f66f676b3cef
Shahab Ejaz <shahabejaz36@gmail.com>
2019-09-11 15:37:51 +0500

    sumAll done

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    
+    if(start < 0 || end < 0) return 'ERROR';    
+
+    if(typeof(start) != 'number' || typeof(end) != 'number') return 'ERROR';
+
+    if(start > end) {
+        let temp = start;
+        start = end;
+        end = temp; 
+    }
+
+    
+
+    sum = 0;
+    for (let index = start; index <= end; index++) {
+        sum += index;
+    }
+
+    return sum;
 }
 
 module.exports = sumAll
