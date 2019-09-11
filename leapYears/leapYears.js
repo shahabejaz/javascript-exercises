0b713aa97af2fff13e290690354f964950b8ea25
Shahab Ejaz <shahabejaz36@gmail.com>
2019-09-11 16:02:22 +0500

    leapYears done

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear = false;
+    if(year % 4 == 0) {
+        isLeapYear = true;
+        if(year % 100 == 0) {
+            isLeapYear = false;
+            if(year % 400 == 0) {
+                isLeapYear = true;
+            }
+        }
+    }
+    return isLeapYear;
 }
 
 module.exports = leapYears
