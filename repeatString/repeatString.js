d160338101f59acb6bdada2ca7f6942b27c527d8
Shahab Ejaz <shahabejaz36@gmail.com>
2019-09-11 12:05:24 +0500

    repeatString done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+    if (count < 0) return "ERROR";
 
+    string = '';
+    for (let index = 0; index < count; index++) {
+        string += word;
+    }
+
+    return string;
 }
 
 module.exports = repeatString
