7ebab4173ddd3068a88f32513c8fee5a3515f0bc
Shahab Ejaz <shahabejaz36@gmail.com>
2019-09-11 17:15:48 +0500

    temConversion done

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let temp = Math.round(5/9 * (fahrenheit - 32) * 10)/10;
+  return temp;
 }
 
-const ctof = function() {
-
+const ctof = function(centigrade) {
+  let temp = Math.round(((centigrade * 9/5) + 32) * 10)/10;
+  return temp;
 }
 
 module.exports = {
