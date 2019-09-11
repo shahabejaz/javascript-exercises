6c3903c744b144c501446acb33ca44fbb0cb2d90
Shahab Ejaz <shahabejaz36@gmail.com>
2019-09-11 15:18:53 +0500

    removeFromArray done

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arguments) {
+    elements = arguments[0];
+    items = Array.prototype.slice.call(arguments,1);
 
+    for (let index = 0; index < items.length; index++) {
+        const element = items[index];
+        remove(elements,element);
+    }
+
+    
+    
+
+    return elements;
 }
+
+function remove(array, element) {
+    const index = array.indexOf(element);
+  
+    if (index !== -1) {
+      array.splice(index, 1);
+    }
+  }
 
 module.exports = removeFromArray
